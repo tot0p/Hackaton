105e6a2af2f11792d442d7212eb620473180178d
tot0p <thomas.lemaitre.78420@gmail.com>
2023-10-03 10:14:51 +0200

    init gin

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "github.com/gin-gonic/gin"
 
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	if err := r.Run(); err != nil {
+		panic(err)
+	}
 }
