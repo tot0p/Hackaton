ff3aa61c9457c7f3df6cdc003d90662770da3655
tot0p <thomas.lemaitre.78420@gmail.com>
2023-10-04 16:10:15 +0200

    add ticket page

--- utils/db/mysql/Tickets.go
+++ utils/db/mysql/Tickets.go
@@ -25,3 +25,50 @@ func CreateTicket(UserUUID, Subject, Content, LinkedData string) (*model.Ticket,
 	}
 	return &tk, nil
 }
+
+// GetAllTickets gets all tickets from the database
+func GetAllTickets() ([]*model.Ticket, error) {
+	rows, err := DB.Conn.Query("SELECT * FROM tickets")
+	if err != nil {
+		return nil, err
+	}
+	var tickets []*model.Ticket
+	for rows.Next() {
+		tk := model.Ticket{}
+		err := rows.Scan(&tk.UUID, &tk.UserUUID, &tk.Subject, &tk.Content, &tk.Status, &tk.LinkedData, &tk.TchatUUID)
+		if err != nil {
+			return nil, err
+		}
+		tickets = append(tickets, &tk)
+	}
+	return tickets, nil
+}
+
+// GetTicketByUUID gets a ticket from the database by uuid
+func GetTicketByUUID(uuid string) (*model.Ticket, error) {
+	tk := model.Ticket{}
+	val := DB.Conn.QueryRow("SELECT * FROM tickets WHERE uuid = ?", uuid)
+	err := val.Scan(&tk.UUID, &tk.UserUUID, &tk.Subject, &tk.Content, &tk.Status, &tk.LinkedData, &tk.CreatedAt, &tk.TchatUUID)
+	if err != nil {
+		return nil, err
+	}
+	return &tk, nil
+}
+
+// GetTicketsByUserUUID gets all tickets from the database by user uuid
+func GetTicketsByUserUUID(userUUID string) ([]*model.Ticket, error) {
+	rows, err := DB.Conn.Query("SELECT * FROM tickets WHERE user_uuid = ?", userUUID)
+	if err != nil {
+		return nil, err
+	}
+	var tickets []*model.Ticket
+	for rows.Next() {
+		tk := model.Ticket{}
+		err := rows.Scan(&tk.UUID, &tk.UserUUID, &tk.Subject, &tk.Content, &tk.Status, &tk.LinkedData, &tk.TchatUUID)
+		if err != nil {
+			return nil, err
+		}
+		tickets = append(tickets, &tk)
+	}
+	return tickets, nil
+}
