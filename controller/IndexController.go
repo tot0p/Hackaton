415a13f291e9d70c8c961ee4ba4940fb559b5e54
tot0p <thomas.lemaitre.78420@gmail.com>
2023-10-05 15:34:25 +0200

    add redirect if not connect on /

--- controller/IndexController.go
+++ controller/IndexController.go
@@ -1,7 +1,17 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"hackaton/utils/session"
+)
 
 func IndexController(ctx *gin.Context) {
-	ctx.HTML(200, "index.html", gin.H{})
+	user := session.SessionsManager.GetUser(ctx)
+	if user == nil {
+		ctx.Redirect(302, "/login")
+		return
+	}
+	ctx.HTML(200, "index.html", gin.H{
+		"user": user,
+	})
 }
