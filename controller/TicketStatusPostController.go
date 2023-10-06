8588dd5d12ed77bd7a9c5e09da9a2b95ee337b02
tot0p <thomas.lemaitre.78420@gmail.com>
2023-10-06 10:09:58 +0200

    update ticket back

--- controller/TicketStatusPostController.go
+++ controller/TicketStatusPostController.go
@@ -1,13 +1,27 @@
 package controller
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"hackaton/model"
 	"hackaton/utils/db/mysql"
+	"hackaton/utils/session"
 	"strconv"
 )
 
 // TicketStatusPostController handles the POST request to change the status of a ticket
 func TicketStatusPostController(ctx *gin.Context) {
+	User := session.SessionsManager.GetUser(ctx)
+	if User == nil {
+		ctx.Redirect(302, "/login")
+		return
+	}
+
+	if User.Role == model.RoleUser {
+		ctx.Redirect(302, "/")
+		return
+	}
+
 	uuid := ctx.Param("uuid")
 	status := ctx.PostForm("status")
 
@@ -16,11 +30,14 @@ func TicketStatusPostController(ctx *gin.Context) {
 		return
 	}
 
+	fmt.Println(status)
+
 	statusInt, err := strconv.Atoi(status)
 	if err != nil {
 		ctx.Redirect(302, "/ticket/"+uuid+"?error=Invalid status")
 		return
 	}
+
 	if statusInt < 0 || statusInt > 2 {
 		ctx.Redirect(302, "/ticket/"+uuid+"?error=Invalid status")
 		return
